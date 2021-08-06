beb568ecf2fa9c9380a0fa22af344392f2d0fd0e
Alexander Fedorov <elf607@ya.ru>
2021-08-06 09:16:01 +0500

    trying to get 3d working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 use crate::systems::bunny_systems::BunnyMoveComponent;
 use crate::systems::bunny_systems::BunnyTTLComponent;
 
-use arcana::{anim::graph, camera::FreeCamera3Controller};
+use arcana::camera::FreeCamera3Controller;
 use rand::Rng;
-use {
-    arcana::*,
-    rapier3d::{
-        dynamics::RigidBodyBuilder,
-        geometry::{Collider, ColliderBuilder},
-        na,
-    },
-};
+use {arcana::*, rapier3d::na};
 
 mod systems {
     pub mod bunny_systems;
@@ -61,9 +54,9 @@ impl Bunny {
                 ttl: rng.gen_range(2.0..16.0),
                 lived: 0.0,
             },
-            Scale {
-                Scale: na::Vector3::new(1.0, 1.0, 1.0),
-            },
+            // Scale {
+            //     Scale: na::Vector3::new(1.0, 1.0, 1.0),
+            // },
         ));
 
         cx.spawner.spawn(async move {
@@ -97,7 +90,7 @@ struct BunnyCount {
 }
 
 fn main() {
-    game3(|mut game: arcana::Game| async move {
+    game3(|mut game| async move {
         game.renderer = Some(Box::new(renderer::vcolor::VcolorRenderer::new(
             &mut game.graphics,
         )?));
@@ -128,10 +121,10 @@ fn main() {
             let bunny = Bunny.spawn(game.cx());
         }
 
-        // game.scheduler
-        //     .add_system(systems::bunny_systems::BunnyMoveSystem);
-        // game.scheduler
-        //     .add_system(systems::bunny_systems::BunnyTTLSystem);
+        game.scheduler
+            .add_system(systems::bunny_systems::BunnyMoveSystem);
+        game.scheduler
+            .add_system(systems::bunny_systems::BunnyTTLSystem);
 
         game.scheduler
             .add_fixed_system(systems::bunny_systems::BunnySpawnSystem, TimeSpan::SECOND);
@@ -145,39 +138,16 @@ fn main() {
             TimeSpan::SECOND,
         );
 
-        // let mut handle = game
+        // game.scheduler.add_system(camera::FreeCameraSystem);
+
+        // let mut object = game
         //     .loader
         //     .load::<assets::object::Object>(
-        //         &"f68f4069-fdd0-4748-9061-ce89ac8f716d".parse().unwrap(),
+        //         &"42f9feac-d11a-4b2f-9c0b-358166237958".parse().unwrap(),
         //     )
         //     .await;
-        // let object = handle.get(&mut game.graphics)?;
 
-        // let mut rng = rand::thread_rng();
-        // let _speed: f32 = rng.gen_range(0.01..0.5);
-
-        // for _ in 0..start {
-        //     let entity = game.world.spawn((
-        //         object.primitives[0].mesh.clone(),
-        //         Global3::new(
-        //             na::Translation3::new(
-        //                 rng.gen_range(-50.0..50.0),
-        //                 0.0,
-        //                 rng.gen_range(-50.0..50.0),
-        //             )
-        //             .into(),
-        //             // na::Translation2::new(-1.0, -1.0).into(),
-        //         ),
-        //         BunnyMoveComponent {
-        //             speed: _speed,
-        //             destination: na::Vector3::new(0.0, 0.0, 0.0),
-        //         },
-        //         BunnyTTLComponent {
-        //             ttl: rng.gen_range(2.0..16.0),
-        //             lived: 0.0,
-        //         },
-        //     ));
-        // }
+        // dbg!(object.get(&mut game.graphics));
 
         Ok(game)
     })
