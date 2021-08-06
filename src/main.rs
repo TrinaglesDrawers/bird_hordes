432430058f15c861923bb669b44101f0a50f5ce3
Alexander Fedorov <elf607@ya.ru>
2021-08-07 04:42:25 +0500

    Moving around some tiles

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crate::systems::bunny_systems::BunnyGridComponent;
 use crate::systems::bunny_systems::BunnyMoveComponent;
 use crate::systems::bunny_systems::BunnyTTLComponent;
 
 use arcana::camera::FreeCamera3Controller;
+use pathfinding;
 use rand::Rng;
 use {arcana::*, rapier3d::na};
 
@@ -13,50 +15,69 @@ mod systems {
 struct Bunny;
 
 impl Bunny {
-    // pub fn new(speed: f32, destination: na::Vector2<f32>) -> Self {
-    //     Bunny { speed, destination }
-    // }
     fn spawn(self, cx: TaskContext<'_>) -> hecs::Entity {
-        // let cat = cx
-        //     .loader
-        //     .load::<assets::ImageAsset>(&"44a9ea43-7f4a-43e1-a7d9-2ee93c6aac84".parse().unwrap());
-
-        // let sampler = cx
-        //     .graphics
-        //     .create_sampler(graphics::SamplerInfo::default())
-        //     .unwrap();
-
-        // // let mut handle = game
-        // //     .loader
-        // //     .load::<assets::object::Object>(
-        // //         &"18d6f877-88e8-46f9-b65d-ae23a4b70588".parse().unwrap(),
-        // //     )
-        // //     .await;
-        // // let object = handle.get(&mut game.graphics);
         let mut handle = cx.loader.load::<assets::object::Object>(
-            &"42f9feac-d11a-4b2f-9c0b-358166237958".parse().unwrap(),
+            // &"0cf76cc1-93f1-47d0-8687-45868725c4fa".parse().unwrap(),
+            &"1c9762a5-26ff-40b9-a47e-b9c5621a771a".parse().unwrap(),
         );
 
+        let mut grid = cx.res.get_mut::<pathfinding::grid::Grid>().unwrap();
+
+        let scales = [
+            arcana::graphics::Scale(na::Vector3::new(0.5, 1.0, 0.5)),
+            arcana::graphics::Scale(na::Vector3::new(1.5, 1.2, 1.5)),
+            arcana::graphics::Scale(na::Vector3::new(0.6, 0.6, 0.6)),
+            arcana::graphics::Scale(na::Vector3::new(1.0, 1.0, 1.0)),
+            arcana::graphics::Scale(na::Vector3::new(1.1, 2.0, 1.1)),
+        ];
         let mut rng = rand::thread_rng();
-        let _speed: f32 = rng.gen_range(0.01..0.5);
+
+        let mut xcoord = rng.gen_range(0..32);
+        let mut ycoord = rng.gen_range(0..32);
+
+        while (!grid.has_vertex(&(xcoord as usize, ycoord as usize))) {
+            xcoord = rng.gen_range(0..32);
+            ycoord = rng.gen_range(0..32);
+        }
+
+        let step = 20.0 / 32.0;
+
+        // let _speed: f32 = rng.gen_range(0.05..0.5);
         let entity = cx.world.spawn((
             self,
             Global3::new(
-                na::Translation3::new(rng.gen_range(-10.0..10.0), 0.0, rng.gen_range(-10.0..10.0))
-                    .into(),
-                // na::Translation2::new(-1.0, -1.0).into(),
+                na::Translation3::new(
+                    step * xcoord as f32 - 10.0,
+                    0.0,
+                    step * ycoord as f32 - 10.0,
+                )
+                .into(),
             ),
             BunnyMoveComponent {
-                speed: _speed,
-                destination: na::Vector3::new(0.0, 0.0, 0.0),
+                speed: 5.0,
+                destination: na::Vector3::new(
+                    step * xcoord as f32 - 10.0,
+                    0.0,
+                    step * ycoord as f32 - 10.0,
+                ),
+                start: na::Vector3::new(
+                    step * xcoord as f32 - 10.0,
+                    0.0,
+                    step * ycoord as f32 - 10.0,
+                ),
+                move_lerp: 0.0,
             },
             BunnyTTLComponent {
                 ttl: rng.gen_range(2.0..16.0),
                 lived: 0.0,
             },
-            // Scale {
-            //     Scale: na::Vector3::new(1.0, 1.0, 1.0),
-            // },
+            BunnyGridComponent {
+                xcoord: xcoord,
+                ycoord: ycoord,
+                hops: Vec::<(i32, i32)>::new(),
+            },
+            // object.primitives[0].mesh.clone(),
+            scales[rng.gen_range(0..scales.len())],
         ));
 
         cx.spawner.spawn(async move {
@@ -65,15 +86,72 @@ impl Bunny {
             let mut cx = AsyncTaskContext::new();
             let cx = cx.get();
 
-            // let handle = handle.get(cx.graphics).unwrap().clone().into_inner();
+            let object = handle.get(cx.graphics).expect(" --- ALARMA! --- ");
 
-            let object = handle.get(cx.graphics).unwrap();
+            cx.world
+                .insert_one(entity, object.primitives[0].mesh.clone());
 
-            // let mut cx = AsyncTaskContext::new();
-            // let cx = cx.get();
+            Ok(())
+        });
 
-            // let handle = handle.get(cx.graphics).unwrap().clone().into_inner();
-            println!("Mesh loaded {}", object.primitives[0].mesh.vertex_count());
+        // grid.remove_vertex(&(xcoord as usize, ycoord as usize));
+
+        entity
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Stone;
+
+impl Stone {
+    fn spawn(self, cx: TaskContext<'_>) -> hecs::Entity {
+        let mut handle = cx.loader.load::<assets::object::Object>(
+            &"0cf76cc1-93f1-47d0-8687-45868725c4fa".parse().unwrap(),
+            // &"1c9762a5-26ff-40b9-a47e-b9c5621a771a".parse().unwrap(),
+        );
+        let mut grid = cx.res.get_mut::<pathfinding::grid::Grid>().unwrap();
+
+        let mut rng = rand::thread_rng();
+
+        let mut xcoord = rng.gen_range(0..32);
+        let mut ycoord = rng.gen_range(0..32);
+
+        while (!grid.has_vertex(&(xcoord as usize, ycoord as usize))) {
+            xcoord = rng.gen_range(0..32);
+            ycoord = rng.gen_range(0..32);
+        }
+
+        let step = 20.0 / 32.0;
+
+        let entity = cx.world.spawn((
+            self,
+            Global3::new(
+                na::Translation3::new(
+                    step * xcoord as f32 - 10.0,
+                    0.075,
+                    step * ycoord as f32 - 10.0,
+                )
+                .into(),
+            ),
+            BunnyGridComponent {
+                xcoord: xcoord,
+                ycoord: ycoord,
+                hops: Vec::<(i32, i32)>::new(),
+            },
+            // object.primitives[0].mesh.clone(),
+            arcana::graphics::Scale(na::Vector3::new(0.25, 0.25, 0.25)),
+        ));
+
+        grid.remove_vertex(&(xcoord as usize, ycoord as usize));
+
+        cx.spawner.spawn(async move {
+            let mut handle = handle.await;
+
+            let mut cx = AsyncTaskContext::new();
+            let cx = cx.get();
+
+            let object = handle.get(cx.graphics).expect(" --- ALARMA! --- ");
+
             cx.world
                 .insert_one(entity, object.primitives[0].mesh.clone());
 
@@ -107,13 +185,54 @@ fn main() {
         game.world
             .insert(
                 game.viewport.camera(),
-                (Global3::identity(), camera::FreeCamera::new()),
+                (
+                    Global3::new(
+                        na::Translation3::new(0.0, 15.0, 20.0)
+                            * na::UnitQuaternion::from_axis_angle(
+                                &na::Vector3::x_axis(),
+                                // std::f32::consts::FRAC_PI_6,
+                                -0.66,
+                            ),
+                    ),
+                    // camera::FreeCamera::new(10.0),
+                ),
             )
             .unwrap();
 
         game.control.add_global_controller(controller1);
 
-        let start = 1000;
+        let mut grid = pathfinding::grid::Grid::new(32, 32);
+        grid.enable_diagonal_mode();
+        grid.fill();
+
+        game.res.insert(grid);
+
+        let mut handle = game
+            .loader
+            .load::<assets::object::Object>(
+                &"0115fcef-c92c-431a-abc6-d4522c95e15a".parse().unwrap(),
+            )
+            .await;
+        let object = handle.get(&mut game.graphics)?;
+        let step = 20.0 / 32.0;
+        for i in 0..32 {
+            for j in 0..32 {
+                game.world.spawn((
+                    object.primitives[0].mesh.clone(),
+                    Global3::new(
+                        na::Translation3::new(step * i as f32 - 10.0, 0.0, step * j as f32 - 10.0)
+                            .into(),
+                    ),
+                    arcana::graphics::Scale(na::Vector3::new(0.25, 0.25, 0.25)),
+                ));
+            }
+        }
+
+        let start = 64;
+
+        for _ in 0..128 {
+            let stome = Stone.spawn(game.cx());
+        }
 
         for _ in 0..start {
             game.res.with(BunnyCount::default).count = start;
--- src/systems/bunny_systems.rs
+++ src/systems/bunny_systems.rs
@@ -4,12 +4,37 @@ use {arcana::*, rapier3d::na};
 
 use crate::Bunny;
 use crate::BunnyCount;
+use pathfinding::prelude::{absdiff, astar};
 
 #[derive(Clone, Debug)]
 pub struct BunnyMoveComponent {
     pub speed: f32,
     pub destination: na::Vector3<f32>,
+    pub start: na::Vector3<f32>,
+    pub move_lerp: f32,
 }
+
+pub struct BunnyGridComponent {
+    pub xcoord: i32,
+    pub ycoord: i32,
+    pub hops: Vec<(i32, i32)>,
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(i32, i32);
+
+// impl Pos {
+//   fn distance(&self, other: &Pos) -> u32 {
+//     (absdiff(self.0, other.0) + absdiff(self.1, other.1)) as u32
+//   }
+
+//   fn successors(&self) -> Vec<(Pos, u32)> {
+//     let &Pos(x, y) = self;
+//     vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
+//          Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
+//          .into_iter().map(|p| (p, 1)).collect()
+//   }
+// }
 
 #[derive(Debug)]
 pub struct BunnyMoveSystem;
@@ -20,27 +45,154 @@ impl System for BunnyMoveSystem {
     }
 
     fn run(&mut self, cx: SystemContext<'_>) -> eyre::Result<()> {
-        for (_entity, (global, movement)) in cx
+        for (_entity, (global, movement, coords)) in cx
             .world
-            .query_mut::<(&mut Global3, &mut BunnyMoveComponent)>()
+            .query_mut::<(
+                &mut Global3,
+                &mut BunnyMoveComponent,
+                &mut BunnyGridComponent,
+            )>()
             .with::<Bunny>()
         {
-            let v = &mut global.iso.translation.vector;
+            let mut v = &mut global.iso.translation.vector;
             let delta = cx.clock.delta.as_secs_f32();
-            let direction =
-                na::Vector2::new(movement.destination.x - v.x, movement.destination.y - v.y)
-                    .normalize();
 
-            v.x = v.x + direction.x * delta * movement.speed;
-            v.z = v.z + direction.y * delta * movement.speed;
+            let step = 20.0 / 32.0;
 
-            let mut rng = rand::thread_rng();
+            movement.move_lerp += delta * movement.speed;
 
-            movement.destination = na::Vector3::new(
-                movement.destination.x + rng.gen_range(-0.05..0.05),
-                0.0,
-                movement.destination.z + rng.gen_range(-0.05..0.05),
-            );
+            // let _v = na::Vector3::new(
+            //     step * coords.xcoord as f32 - 10.0,
+            //     0.0,
+            //     step * coords.ycoord as f32 - 10.0,
+            // );
+            let _v = movement
+                .start
+                .lerp(&movement.destination, movement.move_lerp);
+            // println!("Move lerp = {}", movement.move_lerp);
+
+            // let direction =
+            //     na::Vector2::new(movement.destination.x - v.x, movement.destination.z - v.z)
+            //         .normalize();
+
+            // if (direction != na::Vector2::new(0.0, 0.0)) {
+            //     v.x = v.x + direction.x * delta * movement.speed;
+            //     v.z = v.z + direction.y * delta * movement.speed;
+            // }
+
+            v.x = _v.x;
+            v.y = _v.y;
+            v.z = _v.z;
+
+            // if na::distance(
+            //     &na::Point3::new(v.x, v.y, v.z),
+            //     &na::Point3::new(
+            //         movement.destination.x,
+            //         movement.destination.y,
+            //         movement.destination.z,
+            //     ),
+            // ) <= 0.0000001
+            // {
+            if movement.move_lerp >= 1.0 {
+                let mut grid = cx.res.get_mut::<pathfinding::grid::Grid>().unwrap();
+
+                movement.start = na::Vector3::new(v.x, v.y, v.z);
+                movement.move_lerp = 0.0;
+                if !coords.hops.is_empty() {
+                    // let next_coord = coords.hops.pop().unwrap();
+                    let next_coord = coords.hops.remove(0);
+
+                    if (grid.has_vertex(&(next_coord.0 as usize, next_coord.1 as usize))) {
+                        movement.destination = na::Vector3::new(
+                            step * next_coord.0 as f32 - 10.0,
+                            0.0,
+                            step * next_coord.1 as f32 - 10.0,
+                        );
+
+                        grid.add_vertex((coords.xcoord as usize, coords.ycoord as usize));
+
+                        coords.xcoord = next_coord.0;
+                        coords.ycoord = next_coord.1;
+
+                        grid.remove_vertex(&(next_coord.0 as usize, next_coord.1 as usize));
+                    } else {
+                        if coords.hops.is_empty() {
+                            continue;
+                        }
+                        let goal = Pos(
+                            coords.hops[coords.hops.len() - 1].0,
+                            coords.hops[coords.hops.len() - 1].1,
+                        );
+
+                        let path = astar(
+                            &(coords.xcoord, coords.ycoord),
+                            |p| {
+                                grid.neighbours(&(p.0 as usize, p.1 as usize))
+                                    .into_iter()
+                                    .map(|p| ((p.0 as i32, p.1 as i32), 1))
+                            },
+                            |p| {
+                                grid.distance(
+                                    &(p.0 as usize, p.1 as usize),
+                                    &(goal.0 as usize, goal.1 as usize),
+                                )
+                            },
+                            |p| *p == (goal.0, goal.1),
+                        )
+                        .unwrap_or((Vec::<(i32, i32)>::new(), 0));
+
+                        coords.hops = path.0;
+                        // if (coords.hops.is_empty()) {
+                        //     println!("Path empty1 :(");
+                        // }
+                    }
+                } else {
+                    grid.add_vertex((coords.xcoord as usize, coords.ycoord as usize));
+                    let mut rng = rand::thread_rng();
+
+                    let mut xcoord = rng.gen_range(0..32);
+                    let mut ycoord = rng.gen_range(0..32);
+
+                    while (!grid.has_vertex(&(xcoord as usize, ycoord as usize))) {
+                        xcoord = rng.gen_range(0..32);
+                        ycoord = rng.gen_range(0..32);
+                    }
+
+                    // println!(
+                    //     "Trying to get path between {},{} and {},{}",
+                    //     coords.xcoord, coords.ycoord, xcoord, ycoord
+                    // );
+
+                    let goal = Pos(xcoord, ycoord);
+
+                    let path = astar(
+                        &(coords.xcoord, coords.ycoord),
+                        |p| {
+                            grid.neighbours(&(p.0 as usize, p.1 as usize))
+                                .into_iter()
+                                .map(|p| ((p.0 as i32, p.1 as i32), 0))
+                        },
+                        |p| {
+                            grid.distance(
+                                &(p.0 as usize, p.1 as usize),
+                                &(goal.0 as usize, goal.1 as usize),
+                            )
+                        },
+                        |p| *p == (goal.0, goal.1),
+                    )
+                    // .unwrap();
+                    .unwrap_or((Vec::<(i32, i32)>::new(), 0));
+
+                    coords.hops = path.0;
+                    // if (coords.hops.is_empty()) {
+                    //     println!("Path empty2 :(");
+                    // }
+                    // for _p in &coords.hops {
+                    //     println!("Path: {}, {}", _p.0, _p.1);
+                    // }
+                }
+            }
+            // println!("Position {}", v);
         }
         Ok(())
     }
@@ -54,7 +206,12 @@ impl System for BunnySpawnSystem {
     }
 
     fn run(&mut self, mut cx: SystemContext<'_>) -> eyre::Result<()> {
-        cx.res.with(BunnyCount::default).count += 1;
+        cx.res.with(BunnyCount::default).count += 6;
+        Bunny.spawn(cx.task());
+        Bunny.spawn(cx.task());
+        Bunny.spawn(cx.task());
+        Bunny.spawn(cx.task());
+        Bunny.spawn(cx.task());
         Bunny.spawn(cx.task());
         Ok(())
     }
@@ -77,21 +234,26 @@ impl System for BunnyTTLSystem {
     fn run(&mut self, cx: SystemContext<'_>) -> eyre::Result<()> {
         let mut despawn = BVec::new_in(cx.bump);
 
-        for (_entity, ttl) in cx
+        for (_entity, (ttl, coords)) in cx
             .world
-            .query_mut::<&mut BunnyTTLComponent>()
+            .query_mut::<(&mut BunnyTTLComponent, &BunnyGridComponent)>()
             .with::<Bunny>()
         {
             let delta = cx.clock.delta.as_secs_f32();
             ttl.lived += delta;
 
             if ttl.lived >= ttl.ttl {
-                despawn.push(_entity);
+                despawn.push((_entity, coords.xcoord, coords.ycoord));
             }
         }
 
         for e in despawn {
-            let _ = cx.world.despawn(e);
+            let mut grid = cx.res.get_mut::<pathfinding::grid::Grid>().unwrap();
+
+            grid.add_vertex((e.1 as usize, e.2 as usize));
+
+            let _ = cx.world.despawn(e.0);
+            cx.res.with(BunnyCount::default).count -= 1;
         }
 
         Ok(())
